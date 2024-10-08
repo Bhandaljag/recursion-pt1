429b667cc6c914bd6028373083cda15981bf9b35
Jag <jaggasingh1215@gmail.com>
2024-10-08 08:09:23 -0700

    First time doing recursion, and needed help

--- problems/03-ice-cream-shop.js
+++ problems/03-ice-cream-shop.js
@@ -11,12 +11,35 @@ iceCreamShop(['cookies n cream', 'blue moon', 'superman', 'honey lavender', 'sea
 iceCreamShop(['moose tracks'], 'moose tracks'); // true
 iceCreamShop([], 'honey lavender'); // false
 ***********************************************************************/
+/* I will return a boolean
+my function will take two arguments in the array.
+if argument 2 is in the array(argument 1) then my recursive function will be true
+if not then it will return false. 
+I will have to work my recursive function of array to go to 0; 
+so base case will be array.length of 0
+and then use slice for recursive case. 
+*/
 
 
 function iceCreamShop(flavors, favorite) {
   // Your code here 
-}
+  if (flavors.length === 0){
+    return false;
+  }
+  if (flavors[0] === favorite) {
+    return true;
 
+  }
+  
+  
+  return iceCreamShop(flavors.slice(1), favorite)
+
+}
+console.log(iceCreamShop(['vanilla', 'strawberry'], 'blue moon')); // false
+console.log(iceCreamShop(['pistachio', 'green tea', 'chocolate', 'mint chip'], 'green tea')); // true
+console.log(iceCreamShop(['cookies n cream', 'blue moon', 'superman', 'honey lavender', 'sea salt caramel'], 'pistachio')); // false
+console.log(iceCreamShop(['moose tracks'], 'moose tracks')); // true
+console.log(iceCreamShop([], 'honey lavender')); // false
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
--- problems/04-range.js
+++ problems/04-range.js
@@ -9,12 +9,23 @@ range(1, 5); // [1, 2, 3, 4]
 range(3, 4); // [3]
 range(7, 6); // []
 ***********************************************************************/
-
+/*
+Write a funcftion called range that takes in a start and end number
+Need to return an array of all numbers in that range exclusive 
+if end number is less than start number than return an empty array 
+need to use recursion
+*/
 
 function range(start, end) {
   // Your code here 
+  if ( end <= start ){
+    return [];
+  }
+  return [start].concat(range(start + 1, end))
 }
-
+console.log(range(1, 5)); // [1, 2, 3, 4]
+console.log(range(3, 4)); // [3]
+console.log(range(7, 6)); // []
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
